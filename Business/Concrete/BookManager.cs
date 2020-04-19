b0c9203a3bbffe0c174f0b555774ba93ba851bed
hakanyalp <hakanyavuzalp@hotmail.com>
2020-04-19 23:08:56 +0300

    Project created

---
+++ Business/Concrete/BookManager.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+        IBookDal _bookDal;
+        public BookManager(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+
+        public void Add(Book book)
+        {
+            // Bu kontrol update içinde gerekeceğinden burayı farklı şekilde yönetmek gerek
+            if (_bookDal.Get(filter: b => b.Name == book.Name) == null)
+            {
+                _bookDal.Add(book);
+            }
+            else
+            {
+                throw new Exception(message: "Bu kitap adı zaten mevcut");
+            }
+        }
+
+        public void Delete(Book book)
+        {
+            _bookDal.Delete(book);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _bookDal.GetList().ToList();
+        }
+
+        public List<Book> GetByAuthorId(int id)
+        {
+            return _bookDal.GetList(filter: b => b.AuthorId == id).ToList();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bookDal.Get(filter: b => b.Id == id);
+        }
+
+        public void Update(Book book)
+        {
+            _bookDal.Update(book);
+        }
+    }
+}
